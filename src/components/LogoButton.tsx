d162a7a4517f9dcf01ff520fed6fa3c3cc172526
leendhs <r0943181@student.thomasmore.be>
2025-05-08 14:05:14 +0200

    style: correct size

--- src/components/LogoButton.tsx
+++ src/components/LogoButton.tsx
@@ -1,15 +1,17 @@
 interface Props {
   icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   type: "button" | "submit" | "reset";
+  label?: string;
 }
 
-const LogoButton = ({ icon: Icon, type }: Props) => {
+const LogoButton = ({ icon: Icon, type, label }: Props) => {
   return (
     <button
-      className="flex flex-1 justify-center bg-primary-500/20 border border-primary-500 rounded-lg p-2.5"
       type={type}
+      className="flex flex-1 items-center justify-center gap-2 bg-primary-500/20 border border-primary-500 rounded-lg p-2.5 text-neutral-50 font-medium text-sm"
     >
-      <Icon className="w-6 h-6 fill-current text-primary-500" />
+      <Icon className="w-5 h-5 fill-current text-primary-500" />
+      {label && <span>{label}</span>}
     </button>
   );
 };
