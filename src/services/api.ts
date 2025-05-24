0974aebf1ddae56b99e2652920f98ad35dd15a7f
leendhs <r0943181@student.thomasmore.be>
2025-05-24 09:25:08 +0200

    feat: add api connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 import { useAuthStorage } from "store/authStorage";
 import { User } from "types/user.types";
 
-const API_URL = "http://localhost:3000/api/v1";
+const apiUrl = import.meta.env.VITE_API_URL ?? "http://localhost:3000/api/v1";
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: apiUrl,
   headers: {
     "Content-Type": "application/json",
   },
